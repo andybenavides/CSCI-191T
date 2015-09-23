4302a5a6d1427079844bd616b51cb56400deeb2f
Andy Benavides <andybenavides.22@gmail.com>
2015-09-22 20:59:38 -0700

    commit for exercise 2

--- Presentation16Exercises/exercise2.go
+++ Presentation16Exercises/exercise2.go
@@ -2,8 +2,20 @@ package main
 
 import "fmt"
 
+func findGreatest (num ...int) int{
+	var greatest int
+
+	for _, x := range num{
+		if x > greatest{
+			greatest = x
+		}
+	}
+	return greatest
+}
+
 func main() {
 
-
+	greatest := findGreatest(23, 42, 42, 1, 199)
+	fmt.Println(greatest)
 
 }
