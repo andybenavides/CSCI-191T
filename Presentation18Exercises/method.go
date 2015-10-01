6be747f67a82a31014ee98b18abc8d00927f5991
Andy Benavides <andybenavides.22@gmail.com>
2015-09-30 17:15:53 -0700

    finished method.go

--- Presentation18Exercises/method.go
+++ Presentation18Exercises/method.go
@@ -7,6 +7,18 @@ type food struct{
 	cost int
 }
 
-func main() {
-
+func (f *food) changeCost(newCost int) int{
+	f.cost = newCost
+	return f.cost
 }
+
+func main() {
+	banana := food{"Banana", 2}
+	fmt.Println(banana.cost)
+	banana.changeCost(3)
+	fmt.Println(banana.cost)
+}
+
+// Use a pointer to a food object because the nature of this particular
+// struct is to be mutable.
+// Using a value for the receiver would create a copy and 2 would print twice.
